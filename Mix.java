4564161f57ab72adf6ab8126c74f5e88af5eb2b1
blueberrycola <46426781+blueberrycola@users.noreply.github.com>
2019-04-13 22:24:13 -0400

    fuck vscode

--- Mix.java
+++ Mix.java
@@ -5,8 +5,12 @@ import java.lang.*;
 public class Mix {
 
 	private LinkedList<Character> message;
+
 	private String undoCommands;
-	private Hashtable<Integer, Character> clipBoards;
+	private Hashtable<Integer, String> clipBoards;
+	//This String array is responsible for knowing what commands and the strings affected
+	private ArrayList<String> commandMemory;
+
 
 	private String userMessage;
 	private Scanner scan;
@@ -19,12 +23,13 @@ public class Mix {
 
 		message = new LinkedList<>();
 		clipBoards = new Hashtable<>();
+		commandMemory = new ArrayList<>();
 
 		undoCommands = "";
 	}
 
 	/***********************************************************
-	 * method responcible for getting starter input(the message)
+	 * method responsible for getting starter input(the message)
 	 * @param args: What the user enters in the terminal
 	 **********************************************************/
 	public static void main(String[] args) {
@@ -35,6 +40,7 @@ public class Mix {
 			mix.message.add(mix.userMessage.charAt(i));
 		}
 		mix.mixture();
+		mix.DisplayMessage();
 	}
 
 	/***
@@ -52,31 +58,23 @@ public class Mix {
 			String currUndoCommands = undoCommands;
 
 			try {
-				String command = scan.next("[Qbrpcxh]");
-				boolean scanForSpace = true;
-
-				//checks to see if the command you typed contains two spaces
-				//while(scanForSpace) {
-
-
+				String command = scan.next("[QLHCbrpcxdz]");
 				switch (command) {
 				case "Q":
 					save(scan.next());
-					System.out.println ("Final mixed up message: \"" + message+"\"");
+					System.out.println ("Final mixed up message: \"" + formatLL(message.toString())+"\"");
+					//fixme: Print command instructions for unmix.java
 					System.exit(0);
 				case "b":
-					if(scan.hasNext()) {
-						if(scan.hasNextInt()) {
-							insertbefore(scan.next(), scan.nextInt());
-						} else {
-							replace(scan.next(), scan.next());
-						}
-					}
 					insertbefore(scan.next(), scan.nextInt());
 					break;
 				case "r":
-					//fixme: add if statement for different r commands
-					remove(scan.nextInt(), scan.nextInt());
+					if(scan.hasNextInt()) {
+						remove(scan.nextInt(), scan.nextInt());
+					} else {
+						replace(scan.next(), scan.next());
+					}
+
 					break;
 				case "c":
 					copy(scan.nextInt(), scan.nextInt(), scan.nextInt());
@@ -87,7 +85,7 @@ public class Mix {
 				case "p":
 					paste(scan.nextInt(), scan.nextInt());
 					break;
-				case "h":
+				case "H":
 					helpPage();
 					break;
 				case "d":
@@ -96,10 +94,15 @@ public class Mix {
 				case "z":
 					spazCommand();
 					break;
+				case "L":
+					leetSpeak();
+					break;
+				case "C":
+					countChar(scan.next());
+					break;
 
-					// all the rest of the commands have not been done
-					// No "real" error checking has been done.
 				}
+				undoCommands = message.toString();
 				scan.nextLine();   // should flush the buffer
 				System.out.println("For demonstration purposes only:\n" + undoCommands);
 			}
@@ -112,6 +115,7 @@ public class Mix {
 				message = currMessage;
 			}
 		} while (true);
+
 	}
 
 	/************************************************************************************************************
@@ -121,19 +125,131 @@ public class Mix {
 	 ************************************************************************************************************/
 	private void deleteChars(String c) {
 		char character = c.charAt(0);
+		String unmixInstructions = "";
 		for(int i = 0; i < message.size(); i++) {
 			if(message.get(i) == character) {
+				unmixInstructions = unmixInstructions + i;
 				message.remove(i);
+				i -= 1;
 			}
 		}
+		commandMemory.add("d " + c + " '" + unmixInstructions + "'");
+		//unmixInstructions: insert the char into the elements specified by the index
 	}
 
 	/******************************************************************************
-	 * Does an assortment of commands randomly based on values in the Random object
+	 * A method that simulates random user input by doing an assortment of commands
+	 * all being determined by the random object, which is used in the for loop to
+	 * know how many times we should use commands (5 to 10) and what commands we
+	 * should do (1 to 4) countChar is excluded since it isnt useful for unmix.
 	 *****************************************************************************/
 	private void spazCommand(){
-		//fixme:
+		//fixme: spazCommand()
+		Random rng = new Random();
+		//ASCII_RANDOM: generates a random char decimal between hex values 32 to 126
+		int ASCII_RANDOM = rng.nextInt(126 - 32) + 32;
+		for(int i = 1; i <= rng.nextInt(9 - 1) + 1; i++) {
+			int temp = rng.nextInt(4 - 1) + 1;
+			//insert
+			if(temp == 1) {
+				String randStr = "";
+				int stringLength = rng.nextInt(5 - 1) + 1;
+				//random string builder
+				for(int j = 0; i < stringLength; i++) {
+					randStr = randStr + (char)ASCII_RANDOM;
+				}
+				//llSize made to ensure no out of bounds is used
+				int llSize = message.size() - 1;
+				System.out.println(llSize);
+				insertbefore(randStr, rng.nextInt(llSize - 1) + 1);
+
+			}
+			//replace
+			else if(temp == 2) {
+				String charOne = "" + (char)ASCII_RANDOM;
+				String charTwo = "" + (char)ASCII_RANDOM;
+				replace(charOne, charTwo);
+			}
+			//remove
+			else if(temp == 3) {
+				int min = rng.nextInt((message.size() - 1) + 1);
+				int max = rng.nextInt((message.size() - 1) + 1);
+				//Used to switch min and max in parameters
+				boolean bigMin = false;
+
+				if(min > max) {
+					bigMin = true;
+				}
+
+				if(bigMin) {
+					remove(max, min);
+				} else {
+					remove(min, max);
+				}
+			}
+			//delete
+			else if(temp == 4) {
+				String randomChar = "" + (char)ASCII_RANDOM;
+				deleteChars(randomChar);
+			}
+			//Chase's special touch: 1 out of 100 chance to leetspeak message
+			if(rng.nextInt(100 - 1) + 1 == rng.nextInt(100 - 1) + 1) {
+				leetSpeak();
+			}
+
+		}
 	}
+
+	/**
+	 * Changes message and translates it to leetspeak (the secret language of the DIY/Hacker group: Cult of The Dead Cow)
+	 * multi char letters are separated by a space on both sides
+	 */
+	private void leetSpeak() {
+		String str = message.toString();
+
+		for(int i = 0; i < str.length(); i++) {
+			if (str.contains("A") || str.contains("a")) {
+				replace("A", "@");
+				replace("a", "@");
+			}
+			if(str.contains("C") || str.contains("c")) {
+				replace("C", "(");
+				replace("c", "(");
+			}
+
+			if(str.contains("E") || str.contains("e")) {
+				replace("E", "3");
+				replace("e", "3");
+			}
+
+			if(str.contains("I") || str.contains("i")) {
+				replace("I", "!");
+				replace("i", "!");
+			}
+			if(str.contains("S") || str.contains("s")) {
+				replace("S", "$");
+				replace("s", "$");
+			}
+			if(str.contains("T") || str.contains("t")) {
+				replace("T", "7");
+				replace("t", "7");
+			}
+		}
+
+	}
+	private void countChar(String c) {
+		char character = c.charAt(0);
+		int count = 0;
+		for(int i = 0; i < message.size(); i++) {
+			if(message.get(i) == character) {
+				count++;
+			}
+		}
+		System.out.println("SYSTEM: There are currently " + count + " '" + character + "' character(s)");
+
+	}
+
+
 
 	/**
 	 * Replaces the char requested to a new one that is also requested
@@ -152,6 +268,9 @@ public class Mix {
 					message.add(i, repChar);
 				}
 			}
+			//unmix instructions: switch the actual parameters
+			commandMemory.add("r " + remove + " " + placeholder);
+
 		}
 	}
 
@@ -161,55 +280,74 @@ public class Mix {
 	 * @param stop
 	 */
 	private void remove(int start, int stop) {
-		for(int i = 0; i <= stop - start; i++) {
-			message.remove(start + i);
+		String removedStr = "";
+		for(int i = start; i <= stop; i++) {
+			removedStr = removedStr + message.get(start);
+			message.remove(start);
 		}
+		commandMemory.add("r " + start + " " + stop + " '" + removedStr + "'");
 	}
-
 	/**
-	 * Takes a sequence of chars, places them in the hashtable, and removes the sequence
+	 * Cuts a sequence of char elements in the linked list and copies it to the clipboard HashTable
 	 * from the message
 	 * @param start
 	 * @param stop
-	 * @param clipNum
+	 * @param clipNum the key for the cut string
 	 */
 	private void cut(int start, int stop, int clipNum) {
+		String cut = "";
 		for(int i = start; i <= stop; i++) {
-			clipBoards.put(clipNum, message.get(i));
-		}
-		for(int i = start; i <= stop; i++) {
+			cut = cut + message.get(start);
 			message.remove(start);
 		}
+		clipBoards.put(clipNum, cut);
+		System.out.println("Command: Char sequence saved in clipboard key: " + clipNum);
+		System.out.println("	String: [" + cut + "]");
+		commandMemory.add("x " + start + " " + stop + " " + clipNum + " '" + cut + "'");
 	}
 
-	/**
-	 * Copies the requested range (start to stop) and records it in the hash table
+	/*****************************************************************************************
+	 * Copies the requested range (start to stop) and records it in the hash table as a string
 	 * @param start
 	 * @param stop
 	 * @param clipNum
-	 */
+	 ****************************************************************************************/
 	private void copy(int start, int stop, int clipNum) {
+		String copy = "";
 		for(int i = start; i <= stop; i++) {
-			clipBoards.put(clipNum, message.get(i));
+			copy = copy + message.get(i);
 		}
+		clipBoards.put(clipNum, copy);
+		System.out.println("Command: Char sequence saved in clipboard key: " + clipNum);
+		System.out.println(" 	String: [" + copy + "]");
+		//Copy is not allocated into the hashtable because using it wouldnt have any use for unmix
+
 	}
 
-	/**************************************************
-	 * Places what you have for the given hashtable key
+	/******************************************************************************
+	 * Places what you have for the given hashtable key by recycling insertbefore()
 	 * @param index
 	 * @param clipNum
-	 *************************************************/
+	 *****************************************************************************/
 	private void paste( int index, int clipNum) {
+		//LAZY MODE ACTIVATE:
+		commandMemory.add("p" + " " + index);
+		insertbefore(clipBoards.get(clipNum), index);
 
 	}
          
 	private void insertbefore(String token, int index) {
-
-		//Takes token and makes it a series of char's reversed so it can pop into message easily
-		ArrayList<Character> charList = new ArrayList<>();
+		//Takes token and makes it a series of char's reversed so it can pop into the message LinkedList nice and easily
 		for(int i = 0; i < token.length(); i++) {
 			message.add(index, token.charAt(token.length() - 1 - i));
 		}
+		if(commandMemory.get(commandMemory.size() - 1).charAt(0) == 'p') {
+
+		}else {
+			commandMemory.add("b '" + token + "' " + index);
+		}
+
+		//unmix instructions remove inserted
 	}
 
 	/**
@@ -217,14 +355,49 @@ public class Mix {
 	 */
 	private void DisplayMessage() {
 		System.out.print ("Message:\n");
-		//userMessage = message.toString();
+		String temp = formatLL(message.toString());
+		System.out.println(temp);
+		userMessage = temp;
 
-		for (int i = 0; i < userMessage.length(); i++) 
-			System.out.format ("%3d", i);
+		for (int i = 0; i < userMessage.length(); i++) {
+			System.out.format("%3d", i);
+		}
+
 		System.out.format ("\n");
-		for (char c : userMessage.toCharArray()) 
-			System.out.format("%3c",c);
+		for (char c : userMessage.toCharArray()) {
+			System.out.format("%3c", c);
+		}
 		System.out.format ("\n");
+	}
+
+	/*************************************************************************************************************
+	 * When the linked list gets converted to a string with the toString() java.lang.api version it includes ", "
+	 * which messes with the index in DisplayMessage(). This method fixes that
+	 * @param str
+	 * @return
+	 ************************************************************************************************************/
+	public String formatLL(String str) {
+		String temp = "";
+		for(int i = 1; i < str.length() - 1; i++) {
+			//This if statement makes sure it doesnt skip over commas or spaces included in the message
+			if(str.charAt(i) == ',' || str.charAt(i) == ' ') {
+				if(str.charAt(i) == ',') {
+					if(str.charAt(i + 1) == ',') {
+						temp = temp + str.charAt(i);
+					}
+				}else if(str.charAt(i) == ' ') {
+					if(str.charAt(i + 1) == ' ') {
+						temp = temp + str.charAt(i);
+					}
+				}
+			//Checker for any other char in the LinkedList
+			} else if(str.charAt(i) != ',' && str.charAt(i + 1) == ',') {
+				temp = temp + str.charAt(i);
+			}
+		}
+		//Needed for the last char
+		temp = temp + str.charAt(str.length() - 2);
+		return temp;
 	}
 
 	/**********************************************************
@@ -232,7 +405,8 @@ public class Mix {
 	 * @param filename (the name you want the file to be named)
 	 *********************************************************/
 	public void save(String filename) {
-
+		//FIXME: save() needs to store commands in filename so it can be unmixed
+		//FIXME: save() needs to refactor out of linkedlist.toString()
 		PrintWriter out = null;
 		try {
 			out = new PrintWriter(new BufferedWriter(new FileWriter(filename)));
@@ -241,18 +415,30 @@ public class Mix {
 			e.printStackTrace();
 		}
 
-		out.println(undoCommands);
+		out.println(formatLL(undoCommands));
+		for(int i = 0; i < commandMemory.size(); i++) {
+			out.println(commandMemory.get(i));
+		}
 		out.close();
 	}
+
 
 	/***********************************************************************************************
 	 * A method that prints information on the commands we have for the application and what they do
 	 **********************************************************************************************/
 	private void helpPage() {
 		System.out.println("Commands:");
-		System.out.println("\tQ filename	means, quit! " + " save to filename" );			
-		System.out.println("\t  ~ is used for a space character" );		
-		System.out.println("\t .... etc" );		
-		System.out.println("\th\tmeans to show this help page");
+		System.out.println("\tQ filename	means, quit! " + " save to filename" );
+		System.out.println("\tH\tmeans to show this help page");
+		System.out.println("L means translate your message into leetspeak");
+		System.out.println("b means insert, next string is what you insert, next num is the index you want it at");
+		System.out.println("r means either replace or remove");
+		System.out.println("	r with 2 numbers is remove, r with 2 chars is replace");
+		System.out.println("d means delete, next char is what is removed from the whole linked list");
+		System.out.println("z randomizes user input to scramble your message");
+		System.out.println("p means paste, next two ints are the range(start-end) and the next int is the key for the" +
+				"hashtable");
+		System.out.println("c means copy, next two ints are the range(start-end) and next int is key for the hashtable");
+		System.out.println("x means cut, next two ints are the range(start-end) and the next int is key for the hashtable");
 	}
 }
--- UnMix.java
+++ UnMix.java
@@ -1,18 +1,30 @@
 import java.io.*;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class UnMix {
-	private DoubleLinkedList<Character> message;
+	private LinkedList<Character> message;
 
 	public UnMix() {
-		message = new DoubleLinkedList<Character>();
+		message = new LinkedList();
 	}
 
+	/***************************************************************************************************************
+	 * Takes string arguments as actual parameters that being two things: 1. the filename, 2. Double quoted encoded
+	 * message (The first line in the filename.file) and sends it to unMixture
+	 * @param args
+	 *********************************************************************************************************A****/
 	public static void main(String[] args) {
 	    UnMix v = new UnMix();
 		v.unMixture(args[0], args[1]);
+
 	}
 
+	/***
+	 *
+	 * @param command
+	 * @return
+	 */
 	public String processCommand(String command) {
 		Scanner scan = new Scanner(command);
 		char charInput;
@@ -34,16 +46,39 @@ public class UnMix {
 		return message.toString();
 	}
 
+	/**
+	 * Takes filename and userMessage param and sends it to unMixUsingFile() and returns a string to print
+	 * @param filename
+	 * @param userMessage
+	 */
 	private void unMixture(String filename, String userMessage) {
 		String original = UnMixUsingFile (filename, userMessage);
 		System.out.println ("The Original message was: " + original);
 	}
+	//fixme COMMIT TO GITHUB!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-
+	/**
+	 * Reads the first line and stores it as userMessage
+	 * @param filename
+	 * @param userMessage
+	 * @return
+	 */
 	public String UnMixUsingFile(String filename, String userMessage) {
 		Scanner scanner = null;
 		try {
 			scanner = new Scanner(new File(filename));
+
+			while (scanner.hasNext()) {
+				//String responsible for remembering the string between white lines and parses to int when needed
+				String temp = scanner.nextLine();
+
+
+				System.out.println(temp);
+
+
+
+
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
